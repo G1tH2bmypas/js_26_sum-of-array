ad815992df18953ba06cc263eaf637965e4bd55e
Tekyto <Tekyto2014@gmail.com>
2022-11-17 22:08:42 +0200

    js_26_sum-of-array

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumOfArray(arr) {
-	// write your code here
+	const length = arr.length
+	let sum = 0;
+	for (let i = 0; i < length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 }
 
 module.exports = sumOfArray;
